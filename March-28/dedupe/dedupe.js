9f22b5e26bd61483b92112980497d17b467b9188
alyssamhope <berkeleycole@users.noreply.github.com>
2018-03-29 09:55:48 -0700

    advanced deduce
    added use of spread operator to allow for a varying number of arguments, making the function more flexible.

--- March-28/dedupe/dedupe.js
+++ March-28/dedupe/dedupe.js
@@ -1,30 +1,35 @@
 
-
+// create some arrays to test the function with
 var a = [1, 2, 3];
 var b = [2, 30, 1];
 var c = [2, 4, 5, 6, 7, 0]
 var d = [2, 4, 5, 6, 7, 0]
 
-// function to merge two arrays, takes in two arrays as arguments returns one array with no duplicate values
-function mergeArray(array1, array2, array3, array4) {
+// function mergeArray takes in four arrays as arguments, returns one array with no duplicate values
+function mergeArray(...arrays) {
+	// the spread operator used above (...) allows us to pass in a varying number of parameters to the function
+
     // create empty array to hold new deduped array
     var deduped = [];
 
-    // create one large array to iterate over
-    var combinedArray = array1.concat(array2, array3, array4)
+    // combine all arrays passed to the function into one large array
+    var combinedArray = arrays.shift().concat(...arrays)
+	// console.log(combinedArray);
 
-    // loop through the cokmbined array to add each unique item to deduped array
+    // iterate over the combined array, adding each unique value to deduped array
     for (var index=0; index < combinedArray.length; index++) {
         if(deduped.includes(combinedArray[index]) == false) {
             // add the value to dedupe
             deduped.push(combinedArray[index])
         } else {
             // console log the duplicate value
-            console.log("duplicate found:" combinedArray[index])
+            console.log("duplicate found:" + combinedArray[index])
         }
     }
 }
 
-mergeArray(a, b)
+// mergeArray(a, b)
+//
+// mergeArray(d, c)
 
-mergeArray(d, c)
+mergeArray(a, b, d, c)
